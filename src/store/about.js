3428b8ff98c10cf2a28ed4e0999c714d011eb2f5
Goat95 <hjh9703@naver.com>
2022-12-18 16:24:47 +0900

    Finished Project

---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+import { Store } from "../core/core";
+
+export default new Store({
+  photo: "https://heropy.blog/css/images/logo.png",
+  name: "GOAT95 / KimJunHyeok",
+  email: "goat95@naver.com",
+  blog: "https://heropy.blog",
+  github: "https://github.com/Goat95",
+  repository: "https://github.com/Goat95",
+});
